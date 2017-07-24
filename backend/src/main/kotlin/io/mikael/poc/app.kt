53c604ad68276506c70ce928cb2f1a159461b54a
Mikael Gueck <gumi@iki.fi>
2017-07-24 19:03:28 +0300

    index.html

--- backend/src/main/kotlin/io/mikael/poc/app.kt
+++ backend/src/main/kotlin/io/mikael/poc/app.kt
@@ -1,10 +1,12 @@
 package io.mikael.poc
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.MediaType.TEXT_PLAIN
+import org.springframework.core.io.Resource
+import org.springframework.http.MediaType.*
 import org.springframework.web.reactive.function.server.ServerResponse.ok
 import org.springframework.web.reactive.function.server.router
 
@@ -21,8 +23,14 @@ object Main {
 @Configuration
 class ApplicationRoutes {
 
+    @Value("classpath:/static/index.html")
+    private lateinit var indexHtml: Resource
+
     @Bean
     fun mainRouter() = router {
+        GET("/") {
+            ok().contentType(TEXT_HTML).syncBody(indexHtml)
+        }
         GET("/hello") {
             ok().contentType(TEXT_PLAIN).syncBody("Hello, World!")
         }
