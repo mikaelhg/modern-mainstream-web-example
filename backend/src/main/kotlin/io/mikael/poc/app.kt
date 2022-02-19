693007ac49b640a1ef17e061063f1b8ccd1e7265
Mikael Gueck <gumi@iki.fi>
2022-02-19 15:29:35 +0200

    Counter API.

--- backend/src/main/kotlin/io/mikael/poc/app.kt
+++ backend/src/main/kotlin/io/mikael/poc/app.kt
@@ -2,6 +2,10 @@ package io.mikael.poc
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 object Main {
     @JvmStatic
@@ -12,3 +16,14 @@ object Main {
 
 @SpringBootApplication
 class Application
+
+@RestController
+@RequestMapping("api")
+class ApiController {
+
+    var i = 1
+
+    @GetMapping(path = ["counter"], produces = [APPLICATION_JSON_VALUE])
+    fun counter() = mapOf("counter" to i++)
+
+}
--- backend/src/test/kotlin/io/mikael/poc/integration.kt
+++ backend/src/test/kotlin/io/mikael/poc/integration.kt
@@ -1,16 +1,30 @@
 package io.mikael.poc
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
 
-@SpringBootTest
+@SpringBootTest(classes = [Application::class, ApiController::class, MockTracerConfiguration::class])
+@AutoConfigureMockMvc
 @ActiveProfiles("test")
 class IntegrationTests {
 
-	@Test
-	fun contextLoads() {
+    @Autowired
+    lateinit var mockMvc: MockMvc
 
-	}
+    @Test
+    fun readCounter() {
+        mockMvc.get("/api/counter") {
+            accept = MediaType.APPLICATION_JSON
+        }.andExpect {
+            status { isOk() }
+            content { json("""{"counter":1}""") }
+        }
+    }
 
 }
