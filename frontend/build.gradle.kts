1396e7f14dcad27e5b5d6ebd70a71669aa5fbaa9
Mikael Gueck <gumi@iki.fi>
2022-11-06 16:10:02 +0200

    Frontend build and package.

--- frontend/build.gradle.kts
+++ frontend/build.gradle.kts
@@ -1,7 +1,28 @@
+import com.github.gradle.node.npm.task.NpxTask
+
 plugins {
-    id("com.github.node-gradle.node") version "3.1.1"
+    java
+    id("com.github.node-gradle.node") version "3.5.0"
 }
 
 node {
-    version.set("16.13.2")
+    version.set("18.12.1")
+}
+
+val buildTask = tasks.register<NpxTask>("buildWebapp") {
+    command.set("vite")
+    args.set(listOf("build", "--outDir", "${project.buildDir}/webapp"))
+    dependsOn(tasks.npmInstall)
+    outputs.dir("${project.buildDir}/webapp")
+}
+
+sourceSets {
+    java {
+        main {
+            resources {
+                // This makes the processResources task automatically depend on the buildWebapp one
+                srcDir(buildTask)
+            }
+        }
+    }
 }
