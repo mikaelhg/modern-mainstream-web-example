dcfdf3a7ceaf84d918401fc913b1c6585b852031
Mikael Gueck <gumi@iki.fi>
2025-03-12 02:04:49 +0200

    Upgrade to 2025Q1.

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,13 +1,12 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
 plugins {
-    id("org.springframework.boot") version "3.0.4"
-    id("io.spring.dependency-management") version "1.1.0"
-    id("org.graalvm.buildtools.native") version "0.9.20"
-    kotlin("jvm") version "1.8.0"
-    kotlin("plugin.spring") version "1.8.10"
-    kotlin("plugin.jpa") version "1.8.10"
+    id("org.springframework.boot") version "3.4.3"
+    id("io.spring.dependency-management") version "1.1.7"
+    id("org.graalvm.buildtools.native") version "0.10.5"
+    kotlin("jvm") version "2.1.10"
+    kotlin("plugin.spring") version "2.1.10"
+    kotlin("plugin.jpa") version "2.1.10"
 }
 
 group = "io.mikael.poc"
@@ -15,7 +14,7 @@ version = "0.0.1-SNAPSHOT"
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(19))
+        languageVersion = JavaLanguageVersion.of(21)
     }
 }
 
@@ -30,7 +29,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web") {
         exclude("org.springframework.boot", "spring-boot-starter-tomcat")
     }
-    implementation("io.opentelemetry.instrumentation:opentelemetry-spring-boot-starter:1.19.1-alpha")
+    implementation("io.opentelemetry.instrumentation:opentelemetry-spring-boot-starter:2.13.3")
     implementation("org.springframework.boot:spring-boot-starter-undertow")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
@@ -38,21 +37,14 @@ dependencies {
     runtimeOnly("org.postgresql:postgresql")
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.junit.platform:junit-platform-suite-api:1.9.1")
-    testImplementation("org.junit.platform:junit-platform-suite-engine:1.9.1")
-    testImplementation("org.testcontainers:junit-jupiter:1.17.6")
+    testImplementation("org.junit.platform:junit-platform-suite-api:1.12.0")
+    testImplementation("org.junit.platform:junit-platform-suite-engine:1.12.0")
+    testImplementation("org.testcontainers:junit-jupiter:1.20.6")
 }
 
 tasks.withType<BootBuildImage> {
     buildpacks.set(listOf("gcr.io/paketo-buildpacks/bellsoft-liberica:9",
                           "gcr.io/paketo-buildpacks/java-native-image"))
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "19"
-    }
 }
 
 tasks.withType<Test> {
@@ -63,5 +55,5 @@ tasks.withType<Test> {
 }
 
 tasks.withType<org.springframework.boot.gradle.tasks.bundling.BootJar> {
-    archiveFileName.set("app.jar")
+    archiveFileName = "app.jar"
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,2 @@
-pluginManagement {
-    repositories {
-        maven { url = uri("https://repo.spring.io/milestone") }
-        maven { url = uri("https://repo.spring.io/snapshot") }
-        gradlePluginPortal()
-    }
-}
 rootProject.name = "modern-mainstream-web-example"
 include("backend", "frontend")
